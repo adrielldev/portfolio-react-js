ba4eba02e43a213f424444ca2110288f57bc16ab
adriel <adrieldev@outlook.com>
2022-07-03 12:12:10 -0300

    feature-projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,14 @@ import Nav from "./components/Nav";
 import Banner from './components/Banner'
 import './globals.css'
 import Skills from "./components/Skills";
-
+import Projects from './components/Projects'
 function App() {
   return (
     <>
     <Nav/>
     <Banner/>
     <Skills/>
+    <Projects/>
     </>
   );
 }
---
+++ portfolio/src/components/Projects/dataProjects.js
@@ -0,0 +1,42 @@
+import nukenzieImg from '../../assets/nukenzie.png'
+import kenziehubImg from '../../assets/kenziehub.png'
+import doitImg from '../../assets/doit.png'
+import portfolioImg from '../../assets/portfolio.png'
+
+
+export const data = [
+    {
+        name:'Kenziehub',
+        img:kenziehubImg,
+        description:'A social media to devs register their technologies,projects and intereact with other devs',
+        link:'https://kenziehub-five-kappa.vercel.app/',
+        repo:'https://github.com/adrielldev/kenziehub'
+
+    },
+    {
+        name:'Portfolio',
+        img:portfolioImg,
+        description:'My personal portfolio :) , also the site that you are now',
+        link:'#',
+        repo:'https://github.com/adrielldev/portfolio'
+    },
+    {
+        name:'Doit',
+        img:doitImg,
+        description:'An application to register your tasks',
+        link:'https://doit-nine.vercel.app/',
+        repo:'https://github.com/adrielldev/doit'
+
+    },
+    {
+        name:'Nukenzie',
+        img: nukenzieImg,
+        description:'An app to register the spends and entries in your bank account',
+        link: 'https://nukenzie-flame.vercel.app/',
+        repo:'https://github.com/adrielldev/nukenzie',
+    },  
+
+
+
+
+]
